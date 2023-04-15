c288db83686bcebbe065bccd9c6b5cddbd148ccd
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-15 18:44:33 +0100

    Clean up program
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- YAMLscan/__main__.py
+++ YAMLscan/__main__.py
@@ -1,5 +1,5 @@
 # YAMLScan
-# A tool for scanning files and 
+# A tool for scanning files and
 # Github: https//www.github.com/awesomelewis2007/YAMLscan
 
 import os
@@ -8,7 +8,8 @@ import re
 import yaml
 from colorama import init, Back, Style
 
-def report_findings(yaml,rule, file, line_number):
+
+def report_findings(yaml, rule, file, line_number):
     """
     Report findings to the user
     Opens the rule file and then prints the description and the line number
@@ -19,92 +20,118 @@ def report_findings(yaml,rule, file, line_number):
     """
     if "report" in rule:
         if rule["report"]["colour"] == "red":
-            print(Back.RED + rule["report"]["text"] + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number))
+            print(Back.RED + rule["report"]["text"] + Style.RESET_ALL +
+                  " " + rule["name"] + " in " + file + ":" + str(line_number))
         elif rule["report"]["colour"] == "green":
-            print(Back.GREEN + rule["report"]["text"] + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number))
+            print(Back.GREEN + rule["report"]["text"] + Style.RESET_ALL +
+                  " " + rule["name"] + " in " + file + ":" + str(line_number))
         elif rule["report"]["colour"] == "yellow":
-            print(Back.YELLOW + rule["report"]["text"] + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number))
+            print(Back.YELLOW + rule["report"]["text"] + Style.RESET_ALL +
+                  " " + rule["name"] + " in " + file + ":" + str(line_number))
         elif rule["report"]["colour"] == "blue":
-            print(Back.BLUE + rule["report"]["text"] + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number))
+            print(Back.BLUE + rule["report"]["text"] + Style.RESET_ALL +
+                  " " + rule["name"] + " in " + file + ":" + str(line_number))
         elif rule["report"]["colour"] == "magenta":
-            print(Back.MAGENTA + rule["report"]["text"] + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number))
+            print(Back.MAGENTA + rule["report"]["text"] + Style.RESET_ALL +
+                  " " + rule["name"] + " in " + file + ":" + str(line_number))
         elif rule["report"]["colour"] == "cyan":
-            print(Back.CYAN + rule["report"]["text"] + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number))
+            print(Back.CYAN + rule["report"]["text"] + Style.RESET_ALL +
+                  " " + rule["name"] + " in " + file + ":" + str(line_number))
         elif rule["report"]["colour"] == "white":
-            print(Back.WHITE + rule["report"]["text"] + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number))
+            print(Back.WHITE + rule["report"]["text"] + Style.RESET_ALL +
+                  " " + rule["name"] + " in " + file + ":" + str(line_number))
         else:
-            print(Back.RED + rule["report"]["text"] + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number))
+            print(Back.RED + rule["report"]["text"] + Style.RESET_ALL +
+                  " " + rule["name"] + " in " + file + ":" + str(line_number))
     else:
-        print(Back.RED + "FOUND:" + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number))
+        print(Back.RED + "FOUND:" + Style.RESET_ALL + " " +
+              rule["name"] + " in " + file + ":" + str(line_number))
 
 
-def scan_file(yaml,file):
+def scan_file(yaml, file):
     print("========== [" + file + "] ==========")
+
     with open(file, "rb") as f:
         data = f.read()
+
     for rule in yaml["rules"]:
         if rule["type"] == "string":
             if rule["if"].upper() == "CONTAINS":
                 try:
                     for line_number, line in enumerate(data.splitlines()):
                         if rule["find"].encode() in line:
-                            report_findings(yaml,rule, file, line_number+1)
+                            report_findings(yaml, rule, file, line_number+1)
                 except UnicodeDecodeError:
-                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " +
+                          rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    
             if rule["if"].upper() == "STARTS":
                 try:
                     for line_number, line in enumerate(data.splitlines()):
                         if line.startswith(rule["find"].encode()):
-                            report_findings(yaml,rule, file, line_number+1)
+                            report_findings(yaml, rule, file, line_number+1)
                 except UnicodeDecodeError:
-                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " +
+                          rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    
             if rule["if"].upper() == "ENDS":
                 try:
                     for line_number, line in enumerate(data.splitlines()):
                         if line.endswith(rule["find"].encode()):
-                            report_findings(yaml,rule, file, line_number+1)
+                            report_findings(yaml, rule, file, line_number+1)
                 except UnicodeDecodeError:
-                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " +
+                          rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    
         elif rule["type"] == "regex":
             if rule["if"].upper() == "CONTAINS":
                 try:
                     for line_number, line in enumerate(data.splitlines()):
                         if re.search(rule["find"], line.decode()):
-                            report_findings(yaml,rule, file, line_number+1)
+                            report_findings(yaml, rule, file, line_number+1)
                 except UnicodeDecodeError:
-                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " +
+                          rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    
             if rule["if"].upper() == "STARTS":
                 try:
                     for line_number, line in enumerate(data.splitlines()):
                         if re.search("^" + rule["find"], line.decode()):
-                            report_findings(yaml,rule, file, line_number+1)
+                            report_findings(yaml, rule, file, line_number+1)
                 except UnicodeDecodeError:
-                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " +
+                          rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    
             if rule["if"].upper() == "ENDS":
                 try:
                     for line_number, line in enumerate(data.splitlines()):
                         if re.search(rule["find"] + "$", line.decode()):
-                            report_findings(yaml,rule, file, line_number+1)
+                            report_findings(yaml, rule, file, line_number+1)
                 except UnicodeDecodeError:
-                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " + rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    print(Back.RED + "ERROR:" + Style.RESET_ALL + " " +
+                          rule["name"] + " in " + file + ":" + str(line_number+1) + " (UnicodeDecodeError)")
+                    
         else:
             print("Error: Invalid type in rule set")
             sys.exit(1)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: python3 -m YAMLscan <file> <rule set>")
         sys.exit(1)
+
     else:
         file = sys.argv[1]
         rule_set = sys.argv[2]
 
     with open(rule_set, "r") as f:
         rule_set = yaml.safe_load(f)
-    
+
     if file == "-":
         for root, dirs, files in os.walk("."):
             for file in files:
                 scan_file(rule_set, root+"/"+file)
+                
     else:
         scan_file(rule_set, file)
