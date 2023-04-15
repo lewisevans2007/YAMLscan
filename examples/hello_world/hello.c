e24bbde9a9974653a95e9dfe3adc368353dea2f4
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-15 18:47:19 +0100

    Update hello world example
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- examples/hello_world/hello.c
+++ examples/hello_world/hello.c
@@ -1,3 +1,17 @@
+/* Hello.c
+ *
+ * C Hello World Example for YAMLscan
+ *
+ * Compile this program with gcc and run 
+ * it with YAMLscan.
+ * 
+ * gcc -o hello hello.c
+ * python3 -m yamlscan hello rule.yaml
+ * 
+ * It should print flag Hello world! and 
+ * the printf functions in the file
+*/
+
 #include "stdio.h"
 
 int main(int argc, char **argv) {
